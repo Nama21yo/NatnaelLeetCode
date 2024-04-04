0d79ff5c32b5aece7c6785ae2d784966bf7dee85
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-04-04 12:59:55 +0300

    Time: 2 ms (84.76%), Space: 45.8 MB (81.45%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,21 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j = 0;
-        for(int i = 0;i < nums.length;i++) {
-            if(nums[i]!=0 && nums[j]==0){
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-            }
-            if(nums[j]!=0){
-                j++;
-            }
+        int count = 0;  // Count of non-zero elements 
+  
+        // Traverse the array. If element encountered is 
+        // non-zero, then replace the element at index 'count' 
+        // with this element 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) 
+                nums[count++] = nums[i]; // here count is 
+                                       // incremented             
         }
+
+  
+        // Now all non-zero elements have been shifted to 
+        // front and 'count' is set as index of first 0. 
+        // Make all elements 0 from count to end. 
+        while (count < nums.length) 
+            nums[count++] = 0;
     }
 }
