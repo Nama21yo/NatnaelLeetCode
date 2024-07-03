6852e99b2d432d34beadef8fdd1db49e088f8188
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-07-03 18:36:37 +0300

    Time: 89 ms (17.38%), Space: 18.5 MB (84.52%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
@@ -1,40 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root == null) return new TreeNode(val);
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
         
-        TreeNode cur = root;
-        
-        while(true) {
-            if(cur.val <= val) {
-                if(cur.right != null) cur = cur.right;
-                else {
-                    cur.right = new TreeNode(val);
-                    break;
-                }      
-            } else {
-                if(cur.left != null) cur = cur.left;
-                else {
-                    cur.left = new TreeNode(val);
-                    break;
-                }
-            }
-          
-        }
-        return root;
-    }
-}
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right,val)
+        else:
+            root.left =  self.insertIntoBST(root.left,val)
+        return root
