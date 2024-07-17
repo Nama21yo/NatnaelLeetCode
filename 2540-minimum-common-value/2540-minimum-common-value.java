d865d9a1a6edcb5a3d9b2abd724f44f4a9e7473a
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-07-17 14:16:08 +0300

    Time: 3 ms (35.68%), Space: 62.6 MB (39.02%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,19 +1,29 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        Set<Integer> set = new HashSet<>();
-        int result = -1;
-        for (int num : nums1) {
-            set.add(num);
-        }
+        // Initialize two pointers for each array
+        int i = 0, j = 0;
+        // Initialize result to maximum possible integer value
+        int res = Integer.MAX_VALUE;
         
-        for (int num : nums2)  {
-            if (set.contains(num))  {
-                result = num;
-                break;
+        // Traverse both arrays until one of them is exhausted
+        while (i < nums1.length && j < nums2.length) {
+            // If both elements are equal, update the result
+            if (nums1[i] == nums2[j]) {
+                res = Math.min(res, nums1[i]);
+                i++;
+                j++;
+            }
+            // If the current element in nums1 is smaller, move the pointer in nums1
+            else if (nums1[i] < nums2[j]) {
+                i++;
+            }
+            // If the current element in nums2 is smaller, move the pointer in nums2
+            else {
+                j++;
             }
         }
         
-        return result; 
-        
+        // If no common element was found, return -1, otherwise return the result
+        return res == Integer.MAX_VALUE ? -1 : res;
     }
 }
