09848c7634b18c8f51b519ec06dd49d8078398cf
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-07-30 23:47:33 +0300

    Time: 0 ms (100.00%), Space: 41.4 MB (20.72%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+
+        // The array can be sorted by a single rotation if there is at most one rotation point
+        return count <= 1;
+    }
+}
