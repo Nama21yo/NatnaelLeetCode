6f6948f62151b25a3c6243f75232b7cb72e30e74
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-07-30 23:56:46 +0300

    Time: 1 ms (77.45%), Space: 44.4 MB (86.47%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,24 +1,16 @@
 class Solution {
-    static int removeDuplicates(int[] arr) {
-    
-    int len = 0;
-    for(int i = 1; i< arr.length;i++) {
-      // for(int j=0;j<setss.length;j++) {
-        if(arr[i] != arr[len]) {//1 
-          len++;        
-          arr[len] = arr[i];
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        
+        int k = 0;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[k]) {
+                k++;
+                nums[k] = nums[i];
+            }
         }
-      // }
+        
+        return k + 1;
     }
-    return len+1;
-    } 
-
-  public static void main(String[] args) {
-    int[] ar = {3,3,3,5,6,7}; 
-    int len = removeDuplicates(ar);
-    for(int i = 0; i < len;i++) {
-      System.out.print(ar[i] + " ");  //5 8 4 4 3 6 
-    }
-    System.out.println();
-}
 }
