7157a56c9da60cec48fa71faf2c13e84fbe13abf
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-07-31 00:37:38 +0300

    Time: 2 ms (83.15%), Space: 46.5 MB (9.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int count = 0;  // Count of non-zero elements 
-  
-        // Traverse the array. If element encountered is 
-        // non-zero, then replace the element at index 'count' 
-        // with this element 
+        int j = 0; // Pointer for the position to place the next non-zero element
+        
+        // Move non-zero elements to the beginning of the array
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) 
-                nums[count++] = nums[i]; // here count is 
-                                       // incremented             
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
         }
-
-  
-        // Now all non-zero elements have been shifted to 
-        // front and 'count' is set as index of first 0. 
-        // Make all elements 0 from count to end. 
-        while (count < nums.length) 
-            nums[count++] = 0;
+        
+        // Fill the remaining positions with zeros
+        for (int i = j; i < nums.length; i++) {
+            nums[i] = 0;
+        }
     }
 }
