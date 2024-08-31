d17fbaa59005f7d3660775e8482f2f4160ab0538
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-08-31 14:13:43 +0300

    Time: 1069 ms (10.21%), Space: 47 MB (78.26%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,20 +1,15 @@
-class Solution {
-    public int[] rearrangeArray(int[] nums) {
-        //optimal
-        int[] ans = new int[nums.length];
-        int positiveIndex = 0;
-        int negativeIndex = 1;
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        # optimal
+        ans = [1]*len(nums)
+        positiveIndex = 0
+        negativeIndex = 1
         
-        for(int i = 0; i < nums.length;i++) {
-            if(nums[i] < 0) {
-                ans[negativeIndex] = nums[i];
-                negativeIndex += 2;
-            } else {
-                ans[positiveIndex] = nums[i];
-                positiveIndex += 2;
-            }
-        }
-        
-        return ans;
-    }
-}
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                ans[negativeIndex] = nums[i]
+                negativeIndex += 2
+            else:
+                ans[positiveIndex] = nums[i]
+                positiveIndex += 2
+        return ans
