45a5f8965d7b55dff7ae56c8e4d5b7b62c672ddc
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-09-01 23:07:58 +0300

    Time: 6 ms (56.52%), Space: 43.8 MB (26.08%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,22 +1,27 @@
 class Solution {
+    private class LargerNumberComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            String order1 = a + b;
+            String order2 = b + a;
+            return order2.compareTo(order1);
+        }
+    }
+
     public String largestNumber(int[] nums) {
-        int n = nums.length;
-        String[] s = new String[n];
-        // changed the number into sring
-        for(int i = 0;i<n;i++) {
-            s[i] = String.valueOf(nums[i]);
+        String[] asStrs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            asStrs[i] = String.valueOf(nums[i]);
         }
-        
-        Arrays.sort(s,(a,b) -> (b+a).compareTo(a+b));
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for(String str: s) {
-            sb.append(str);
+        Arrays.sort(asStrs, new LargerNumberComparator());
+        if (asStrs[0].equals("0")) {
+            return "0";
         }
-        
-        String result = sb.toString();
-        
-        return result.startsWith("0") ? "0" : result;
+        String largestNumberStr = new String();
+        for (String numAsStr : asStrs) {
+            largestNumberStr += numAsStr;
+        }
+
+        return largestNumberStr;
     }
 }
