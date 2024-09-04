1cfb395a0a8fa2ec08c6e79f8d1ca93e543f266d
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-09-05 02:11:03 +0300

    Time: 0 ms (100.00%), Space: 41.5 MB (97.06%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,27 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    // Main function to generate Pascal's Triangle up to numRows
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ans = new ArrayList<>(); // List to store all rows of Pascal's Triangle
-        for (int i = 1; i <= numRows; i++) { // Loop through each row
-            ans.add(generateRow(i)); // Generate each row and add to the list
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        for(int i = 1; i <= numRows; i++) {
+            ans.add(generateRow(i));
         }
-        return ans; // Return the final list containing all rows
+        return ans;
     }
-
-    // Helper function to generate a specific row of Pascal's Triangle
+    
     private List<Integer> generateRow(int row) {
-        int ans = 1; // Initialize the first value of the row
-        List<Integer> ansRow = new ArrayList<>(); // List to store the current row
-        ansRow.add(1); // Add the first element which is always 1
-
-        for (int col = 1; col < row; col++) { // Loop through each column in the row
-            ans = ans * (row - col); // Calculate the current element using binomial coefficient
-            ans = ans / col;
-            ansRow.add(ans); // Add the calculated element to the current row list
+        List<Integer> rows = new ArrayList<>();
+        int first = 1;
+        rows.add(first);
+        for(int i = 1; i < row; i++) {
+            first = first * (row - i);
+            first = first / i;
+            rows.add(first);
         }
-        return ansRow; // Return the current row list
+        
+        return rows;
     }
 }
