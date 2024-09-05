9dec3c31c1d4f4d5f218b3f0678920dddb24df7c
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-09-05 16:03:34 +0300

    Time: 108 ms (12.58%), Space: 17.9 MB (21.39%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,16 +1,47 @@
 class Solution:
     def majorityElement(self, nums):
-        countEl = {}
+        candidates = self.findCandidate(nums)
+        return self.validateCandidates(nums, candidates)
+    
+    def findCandidate(self, nums):
+        count1, count2 = 0, 0
+        cand1, cand2 = None, None
+        
+        for num in nums:
+            if cand1 == num:
+                count1 += 1
+            elif cand2 == num:
+                count2 += 1
+            elif count1 == 0 and num != cand2:
+                cand1 = num
+                count1 = 1
+            elif count2 == 0 and num != cand1:
+                cand2 = num
+                count2 = 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        
+        return [cand1, cand2]
+    
+    def validateCandidates(self, nums, candidates):
+        count1, count2 = 0, 0
+        cand1, cand2 = candidates[0], candidates[1]
+        
+        # Count occurrences of the two candidates
+        for num in nums:
+            if num == cand1:
+                count1 += 1
+            elif num == cand2:
+                count2 += 1
+        
         ans = []
         mini = len(nums) // 3
         
-        # Count the occurrences of each element
-        for num in nums:
-            countEl[num] = countEl.get(num, 0) + 1
-        
-        # Add elements that appear more than len(nums) // 3 times
-        for num, count in countEl.items():
-            if count > mini:
-                ans.append(num)
+        # Only add the candidates if their counts exceed n/3
+        if count1 > mini:
+            ans.append(cand1)
+        if count2 > mini and cand2 != cand1:  # Avoid duplicates
+            ans.append(cand2)
         
         return ans
