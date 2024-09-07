d754e86f04a48304e7cf1bc07c5f2197bc7c8438
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-09-07 13:44:54 +0300

    Time: 1 ms (36.20%), Space: 41.9 MB (91.84%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,35 +1,53 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] nums = new int[n + m];
-        int left = 0;
-        int right = 0;
-        int index = 0;
+     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int len = n + m; // Total length of both arrays
+        int gap = (len / 2) + (len % 2); // Initial gap
 
-        // Merging nums1 and nums2 into nums
-        while (left < m && right < n) {
-            if (nums1[left] <= nums2[right]) {
-                nums[index] = nums1[left];
+        while (gap > 0) {
+            int left = 0;
+            int right = left + gap;
+
+            while (right < len) {
+                // Case 1: nums1 and nums2 (left is in nums1, right is in nums2)
+                if (left < m && right >= m) {
+                    if (nums1[left] > nums2[right - m]) {
+                        // Swap between nums1 and nums2
+                        int temp = nums1[left];
+                        nums1[left] = nums2[right - m];
+                        nums2[right - m] = temp;
+                    }
+                }
+                // Case 2: nums2 and nums2 (both left and right are in nums2)
+                else if (left >= m) {
+                    if (nums2[left - m] > nums2[right - m]) {
+                        // Swap within nums2
+                        int temp = nums2[left - m];
+                        nums2[left - m] = nums2[right - m];
+                        nums2[right - m] = temp;
+                    }
+                }
+                // Case 3: nums1 and nums1 (both left and right are in nums1)
+                else {
+                    if (nums1[left] > nums1[right]) {
+                        // Swap within nums1
+                        int temp = nums1[left];
+                        nums1[left] = nums1[right];
+                        nums1[right] = temp;
+                    }
+                }
+
                 left++;
-            } else {
-                nums[index] = nums2[right];
                 right++;
             }
-            index++;
+
+            // Reduce the gap for the next iteration
+            if (gap == 1) break;
+            gap = (gap / 2) + (gap % 2);
         }
 
-        // Copy remaining elements of nums1 if any
-        while (left < m) {
-            nums[index++] = nums1[left++];
-        }
-
-        // Copy remaining elements of nums2 if any
-        while (right < n) {
-            nums[index++] = nums2[right++];
-        }
-
-        // Copy merged elements back to nums1
-        for (int i = 0; i < (n + m); i++) {
-            nums1[i] = nums[i];
+        // Now merge nums2 into nums1 after sorting
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
         }
     }
 }
