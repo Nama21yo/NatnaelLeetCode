0257281a68fae98cc5371c595c5665a15e313b4a
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-09-18 21:17:34 +0300

    Time: 12 ms (70.29%), Space: 44.4 MB (68.4%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,17 +1,15 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int left = 0;
+      
         int right = 0;
-        int lastSeen[] = new int[3];
+        int lastSeen[] = {-1,-1,-1};
         int count = 0;
         
         while(right < s.length()) {
-            lastSeen[s.charAt(right) - 'a']++;
+            lastSeen[s.charAt(right) - 'a'] = right;
            
-            while(lastSeen[0] > 0 && lastSeen[1] > 0 && lastSeen[2] > 0) {
-                count += (s.length() - right);
-                lastSeen[s.charAt(left) - 'a']--;
-                left++;
+            if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
+                count += 1 + Math.min(Math.min(lastSeen[0],lastSeen[1]),lastSeen[2]);
             }
             right++;
         }
