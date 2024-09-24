f646d324977cc3a6cf39f20e7a3e820d46ae6d4c
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-09-24 12:25:17 +0300

    Time: 24 ms (73.91%), Space: 46.5 MB (34.19%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,24 +1,16 @@
-import java.util.HashMap;
-
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> arr_sums = new HashMap<>();
-        arr_sums.put(0, 1);  // Initialize the map with sum 0 having one occurrence
-        int sum = 0;  // This variable will keep the cumulative sum
-        int result = 0;  // This will store the number of subarrays that sum to k
-        
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];  // Update the cumulative sum
-            
-            // Check if there is a subarray (with sum k) ending at index i
-            if (arr_sums.containsKey(sum - k)) {
-                result += arr_sums.get(sum - k);
+        HashMap<Integer,Integer> prefixSumCount= new HashMap<>();
+        prefixSumCount.put(0,1);
+        int prefixSum = 0;
+        int count = 0;
+        for(int num: nums) {
+            prefixSum += num;
+            if(prefixSumCount.containsKey(prefixSum - k)) {
+                count += prefixSumCount.get(prefixSum - k);
             }
-            
-            // Update the map with the current sum
-            arr_sums.put(sum, arr_sums.getOrDefault(sum, 0) + 1);
+        prefixSumCount.put(prefixSum,prefixSumCount.getOrDefault(prefixSum,0) + 1);
         }
-        
-        return result;
+        return count;
     }
 }
