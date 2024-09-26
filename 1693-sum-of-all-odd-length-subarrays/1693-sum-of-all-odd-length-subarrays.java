7bcd5d52858b0183d44f318c21607691297a9e63
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-09-26 13:53:18 +0300

    Time: 0 ms (100%), Space: 41.5 MB (28.91%) - LeetHub

--- 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
@@ -1,22 +1,14 @@
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
+        int oddSum = 0;
         int n = arr.length;
-        int entire_sum = 0;
-
-        // Outer loop to select the starting point of subarrays
-        for (int i = 0; i <= n - 1; i++) {
-            int sum = 0;
-
-            // Inner loop to select the end point of subarrays
-            for (int j = i; j <= n - 1; j++) {
-                sum += arr[j];  // Add the current element to the sum
-
-                // Only add to entire_sum if the length of the subarray is odd
-                if ((j - i + 1) % 2 != 0) {  // Length of subarray = j - i + 1
-                    entire_sum += sum;
-                }
-            }
+        for(int i = 0; i < n; i++) {
+            int start = i + 1;
+            int end = n - i;
+            int total = start * end;
+            int oddLength = total % 2 == 0 ? total / 2: (total / 2) + 1;
+            oddSum += oddLength * arr[i];
         }
-        return entire_sum;
+        return oddSum;
     }
 }
