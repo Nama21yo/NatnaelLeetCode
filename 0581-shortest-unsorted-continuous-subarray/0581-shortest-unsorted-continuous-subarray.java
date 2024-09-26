203d2929f12c659c11816c64b483da5f6e489a4e
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-09-26 13:07:55 +0300

    Time: 1 ms (96.79%), Space: 46 MB (5.08%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
@@ -1,23 +1,45 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        int[] b = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(b);
+        int n = nums.length;
+        int min_unsorted = Integer.MAX_VALUE;
+        int max_unsorted = Integer.MIN_VALUE;
 
-        int i = 0;
-        while (i < nums.length && nums[i] == b[i]) {
-            i++;
+        // First pass: Find the minimum and maximum elements that are out of order
+        for (int i = 0; i < n; i++) {
+            if (isOutOfBound(nums, i)) {
+                min_unsorted = Math.min(min_unsorted, nums[i]);
+                max_unsorted = Math.max(max_unsorted, nums[i]);
+            }
         }
 
-        int j = nums.length - 1;
-        while (j >= 0 && nums[j] == b[j]) {
-            j--;
-        }
-
-        // If i becomes equal to nums.length, it means the array is already sorted
-        if (i > j) {
+        // If the array is already sorted, return 0
+        if (min_unsorted == Integer.MAX_VALUE) {
             return 0;
         }
 
-        return (j - i + 1);
+        // Second pass: Find the correct position to place the min_unsorted and max_unsorted
+        int start = 0;
+        while (start < n && nums[start] <= min_unsorted) {
+            start++;
+        }
+
+        int end = n - 1;
+        while (end >= 0 && nums[end] >= max_unsorted) {
+            end--;
+        }
+
+        return end - start + 1;
+    }
+
+    private boolean isOutOfBound(int[] nums, int i) {
+        // Compare it to previous and next elements
+        if(nums.length == 1) return false;
+        if (i == 0) {
+            return nums[i] > nums[i + 1];
+        } else if (i == nums.length - 1) {
+            return nums[i] < nums[i - 1];
+        } else {
+            return nums[i] < nums[i - 1] || nums[i] > nums[i + 1];
+        }
     }
 }
