c404e5556e777ec5c855189e7950a1bbcfb2e37c
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-09-28 18:02:48 +0300

    Time: 0 ms (100%), Space: 42.2 MB (27.59%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,37 +1,32 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int m = matrix.length;
-
-        // Step 1: Transpose the matrix (swap elements across the diagonal)
-        for (int i = 0; i < m; i++) {
-            for (int j = i + 1; j < m; j++) {
+        //Transpose the array
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for(int i = 0; i < row; i++) {
+            for(int j = i + 1; j < col;j++){
                 swap(matrix, i, j);
             }
         }
-
-        // Step 2: Reverse each row
-        for (int i = 0; i < m; i++) {
+        // Reverse the array
+        for(int i = 0; i < row; i++) {
             reverse(matrix[i]);
         }
     }
-
-    // Helper method to swap elements at (i, j) and (j, i)
-    private void swap(int[][] matrix, int i, int j) {
-        int temp = matrix[i][j];
-        matrix[i][j] = matrix[j][i];
-        matrix[j][i] = temp;
-    }
-
-    // Helper method to reverse a row
     private void reverse(int[] row) {
         int start = 0;
         int end = row.length - 1;
-        while (start < end) {
+        while(start <= end) {
             int temp = row[start];
             row[start] = row[end];
             row[end] = temp;
             start++;
             end--;
         }
+    }
+    private void swap(int matrix[][],int i, int j) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
     }
 }
