7a37a88bb897a62014dfbf9a059cd2498d918c38
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-09-30 10:15:07 +0300

    Time: 117 ms (25.31%), Space: 64.1 MB (11.49%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,21 +1,27 @@
 class Solution {
     public int countPrimes(int n) {
-        if (n <= 2) return 0;
-        
-        boolean[] composite = new boolean[n];
-        for (int i = 2; i * i < n; i++) {
-            if (composite[i] == false) {
-                for (int j = i * i; j < n; j += i) {
-                    composite[j] = true;
+        int primes[] = new int[n + 1];
+        for(int i = 2; i < n; i++) {
+           primes[i] = 1;
+        }
+        // primes[0] = 0;
+        // primes[1] = 0;
+
+        //sieve of Erathoneses
+        for(int i = 2; i * i <= n;i++) {
+            if(primes[i] == 1) {
+                for(int j = 2; i * j <= n;j++) {
+                    primes[i * j] = 0;
                 }
             }
         }
-        
-        int primeCount = 0;
-        for (int i = 2; i < n; i++) {
-            if (composite[i] == false) primeCount++;
+        int count = 0;
+        for(int i = 2; i < n; i++) {
+            if(primes[i] == 1) count++;
         }
         
-        return primeCount;
+        return count;
     }
+
+    
 }
