e5c05bfdd0e8d7c77b448953ded923acc8c85c17
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-09-30 10:47:10 +0300

    Time: 36 ms (56%), Space: 16.4 MB (85.56%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n == 0:
+            return False
+        if n % 2 == 0:
+            return self.isUgly(n // 2)
+        if n % 3 == 0:
+            return self.isUgly(n // 3)
+        if n % 5 == 0:
+            return self.isUgly(n // 5)
+        if n > 0 and n <= 3:
+            return True
+        return False
+        
+        
