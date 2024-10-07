da388c8a22b2e4af837b1a23a0ab9efc9fb04a7e
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-10-07 23:16:42 +0300

    Time: 4 ms (84.74%), Space: 43.2 MB (60.68%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -1,20 +1,17 @@
 class Solution {
     public int minLength(String s) {
-        Deque<Character> stack = new LinkedList();
-
-        for(int i = 0; i < s.length();++i) {
-            char c = s.charAt(i);
-            if(stack.isEmpty()) {
-                stack.addLast(c);
-            } else if(c == 'B' && stack.peekLast() =='A') {
-                stack.pollLast();
-            } else if(c == 'D' && stack.peekLast() == 'C') {
-                stack.pollLast();
-            } else {
-                stack.addLast(c);
-            }
+       Stack<Character> stack = new Stack<>();
+        // O(n) time and O(n) space
+        for (char c : s.toCharArray()) {
+           if(c == 'B' && !stack.isEmpty() && stack.peek() == 'A') {
+                stack.pop();
+           } else if(c == 'D' && !stack.isEmpty() && stack.peek() == 'C') {
+                stack.pop();
+           } else {
+                stack.push(c);
+           }
         }
-
+        
         return stack.size();
     }
 }
