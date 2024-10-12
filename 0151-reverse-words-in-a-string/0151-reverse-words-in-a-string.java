50bd4448e3e9fe87ad0918d4ca34e403712acc7f
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-10-12 16:46:43 +0300

    Time: 7 ms (64.44%), Space: 42.6 MB (92.46%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,8 +1,64 @@
 class Solution {
     public String reverseWords(String s) {
-        s = s.trim();
-        List<String> wordList = Arrays.asList(s.split("\\s+"));
-        Collections.reverse(wordList);
-        return String.join(" ", wordList);
+        // Step 1: Remove leading, trailing, and extra spaces
+        s = trimSpaces(s);
+        
+        // Step 2: Convert the string to a character array and reverse the entire string
+        char[] words = s.toCharArray();
+        int n = words.length;
+        reverse(words, 0, n - 1);
+
+        // Step 3: Reverse each word in the reversed string
+        int p1 = 0;
+        while (p1 < n) {
+            int p2 = p1;
+            while (p2 < n && words[p2] != ' ') {
+                p2++;
+            }
+            reverse(words, p1, p2 - 1);
+            p1 = p2 + 1;
+        }
+
+        return new String(words);
+    }
+
+    // Helper method to reverse a portion of the character array
+    private void reverse(char[] s, int l, int r) {
+        while (l < r) {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
+
+    // Helper method to trim spaces: remove leading, trailing and extra spaces
+    private String trimSpaces(String s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+
+        // Skip leading spaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // Process characters and collapse multiple spaces
+        while (i < n) {
+            if (s.charAt(i) != ' ') {
+                sb.append(s.charAt(i));
+            } else if (sb.length() > 0 && sb.charAt(sb.length() - 1) != ' ') {
+                sb.append(' ');
+            }
+            i++;
+        }
+
+        // Remove any trailing space
+        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ' ') {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        return sb.toString();
     }
 }
