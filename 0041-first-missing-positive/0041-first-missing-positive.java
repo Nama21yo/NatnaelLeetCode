922c21e1e43f50947308a001433c7a046b5d0bc5
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-10-15 23:53:44 +0300

    Time: 2 ms (43.75%), Space: 57.5 MB (31.5%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -2,17 +2,16 @@ class Solution {
    public int firstMissingPositive(int[] nums) {
         // O(3n) and O(1) 
         if(nums.length==0) return 1;
+
         int lastPositiveindex = partition(nums);
         
         for(int i=0;i<lastPositiveindex;i++){
             int k = Math.abs(nums[i]);
             if(1 <= k && k <= lastPositiveindex) {
                 if(nums[k - 1] > 0) {
-                    nums[k - 1] = -nums[k - 1];
+                    nums[k - 1] = - nums[k - 1];
                 }
             }
-            // if(k <= lastPositiveindex)
-            //     nums[k-1]=(nums[k-1]<0)?nums[k-1]:-nums[k-1];
         }
         for(int i= 1;i < lastPositiveindex + 1;i++){
             if(nums[i - 1] >= 0){
@@ -23,14 +22,14 @@ class Solution {
     }
 
     private int partition(int[] nums) {
-        int j = 0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] > 0){
-                swap(nums, i , j);
-                j++;
+        int l = 0;
+        for(int r = 0; r < nums.length; r++){
+            if(nums[r] > 0){
+                swap(nums, r , l);
+                l++;
             }
         }
-        return j;
+        return l;
     }
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
