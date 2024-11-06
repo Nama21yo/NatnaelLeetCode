fe4ffd86026e72e0c64ee210c4c3aeffc608ca9f
Nama21yo <121098756+Nama21yo@users.noreply.github.com>
2024-11-07 00:06:12 +0300

    Time: 20 ms (80%), Space: 11.8 MB (38.46%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.py
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.py
@@ -1,30 +1,27 @@
-class Solution:
-    def canSortArray(self, nums: List[int]) -> bool:
-        # Bubble Sorting
-        n = len(nums)
-        for i in range(n):
-            flag = False
+class Solution(object):
+    def canSortArray(self, nums):
+        previousMax = float("-inf")
+        currentMax = nums[0]
+        currentMin = nums[0]
 
-            for j in range(n - i - 1):
-                if(nums[j] <= nums[j + 1]):
-                    continue
-                if(self.setBits(nums[j]) == self.setBits(nums[j + 1])):
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
-                    flag = True
-                else:
+        for num in nums:
+            if self.setBits(num) == self.setBits(currentMin):
+                currentMin = min(currentMin, num)
+                currentMax = max(currentMax, num)
+            else:
+                if currentMin < previousMax:
                     return False
-            if not flag:
-                break
+                previousMax = currentMax
+                currentMin = num
+                currentMax = num
+        if currentMin < previousMax:
+                return False
         return True
-    
+        
     def setBits(self, number):
         count = 0
         while(number > 0):
-            # if(number % 2 == 1):
-            #     count += 1
             count += number & 1
-            # number = number // 2
             number >>= 1
         return count
-
         
