2b6d5bffa117fc9744a7a9afcf55f8b2931de9ad
Natnael Yohanes <mataniayohanes@gmail.com>
2024-11-27 20:00:37 +0300

    Time: 0 ms (100%), Space: 17.3 MB (6.11%) - LeetHub

---
+++ 0886-score-of-parentheses/0886-score-of-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:  
+    def scoreOfParentheses(self, s: str) -> int:  
+        count = 0  
+        stack = []  
+        
+        for c in s:  
+            if c == '(':  
+                stack.append(0)  
+            else:   
+                val = 0   
+                while stack and stack[-1] != 0:  
+                    val += stack.pop()    
+                if stack:  
+                    stack.pop()  
+                val = max(2 * val, 1)    
+                stack.append(val)  
+         
+        while stack:  
+            count += stack.pop()  
+        
+        return count  
