e0546264517143392952c8b31cfe82fd1d6dcfa9
Natnael Yohanes <mataniayohanes@gmail.com>
2024-12-18 21:03:53 +0300

    Time: 0 ms (100%), Space: 17.8 MB (11.35%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closeOpen = {")":"(","}":"{","]":"["}
+        stack = []
+
+        for char in s:
+            # If it is Open parenthesis
+            if char not in closeOpen:
+                stack.append(char)
+            else:
+                # my stack is empty
+                if not stack:
+                    return False
+                popped = stack.pop()
+                if popped != closeOpen[char]:
+                    return False
+        return not stack
+        
+        
+        
