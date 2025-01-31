1e21dcf5ea97983a2a6ef41a01aa04da103cb6b5
Natnael Yohanes <mataniayohanes@gmail.com>
2025-01-31 17:07:15 +0300

    Time: 66 ms (5.55%), Space: 17.8 MB (6.31%) - LeetHub

---
+++ 3651-transformed-array/3651-transformed-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        result = []
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                result.append(nums[i])
+            else:
+                # Remainder is best when it is circular array
+                result.append(nums[(nums[i] + i) % len(nums)])
+        return result
