a85d9cda5cf5997a8232ea0bd1369aecd7b23eee
Natnael Yohanes <mataniayohanes@gmail.com>
2025-01-31 14:31:17 +0300

    Time: 0 ms (100%), Space: 17.9 MB (11.07%) - LeetHub

---
+++ 2645-pass-the-pillow/2645-pass-the-pillow.py
@@ -0,0 +1,10 @@
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        full_iteration = time // (n - 1)
+
+        back_iteration = time % (n - 1)
+
+        if full_iteration % 2 == 0:
+            return back_iteration + 1 
+        else:
+            return n - back_iteration  
