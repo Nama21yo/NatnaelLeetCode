d46af2c4bb0128790b57113424871ea287fd8f07
Natnael Yohanes <mataniayohanes@gmail.com>
2025-02-03 12:03:34 +0300

    Time: 41 ms (32.03%), Space: 17.6 MB (55.99%) - LeetHub

---
+++ 1449-print-words-vertically/1449-print-words-vertically.py
@@ -0,0 +1,21 @@
+class Solution:
+    def printVertically(self, s: str) -> List[str]:
+        # s = s.strip()
+        split_space = s.split()
+        maxi = 0
+        for string in split_space:
+            maxi = max(maxi, len(string))
+        
+        ans = []
+        m = 0
+        for i in range(maxi):
+            helper = []
+            for j in range(len(split_space)):
+                if i >= len(split_space[j]):
+                    helper.append(" ")
+                else:
+                    helper.append(split_space[j][i])
+            ans.append("".join(helper).rstrip())
+        return ans
+
+        
