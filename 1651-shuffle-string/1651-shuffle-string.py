9322e3c0dda594b8d8302dcfc8464edb9e20cb6d
Natnael Yohanes <mataniayohanes@gmail.com>
2025-02-05 19:19:28 +0300

    Time: 0 ms (100%), Space: 17.7 MB (58.35%) - LeetHub

---
+++ 1651-shuffle-string/1651-shuffle-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        shuffle_string = list(s)
+        n = len(shuffle_string)
+        for i in range(n):
+            val = indices[i]
+            shuffle_string[val]  = s[i]
+        return "".join(shuffle_string)
