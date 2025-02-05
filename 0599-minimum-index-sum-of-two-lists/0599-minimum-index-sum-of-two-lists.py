a138663aaec4b8b8e87d93f26db142716d13599e
Natnael Yohanes <mataniayohanes@gmail.com>
2025-02-05 18:07:09 +0300

    Time: 2 ms (96.52%), Space: 18 MB (77.5%) - LeetHub

---
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        common_string = defaultdict(int)
+
+        for i,string in enumerate(list1):
+            common_string[string] = i
+          
+        
+        min_index = float("inf")
+        ans = []
+        for i, string in enumerate(list2):
+            if string in common_string:
+                index_sum = i + common_string[string]
+                if index_sum < min_index:
+                    min_index = index_sum
+                    ans = [string] # found my least sum index
+                    # intial it again with new string forgetting the rest
+                elif index_sum == min_index:
+                    ans.append(string)
+        
+        return ans
