2a19482c133ff5b80f87f51bb9ba95d376b0d1ce
Natnael Yohanes <mataniayohanes@gmail.com>
2025-02-10 16:12:03 +0300

    Time: 151 ms (12.68%), Space: 19.2 MB (7.95%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,18 +1,19 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         n = len(heights)
-        # Insertion Sort
-        # first part is sorted
-        for i in range(1,n):
-            key_height = heights[i]
-            key_name = names[i]
 
-            j = i - 1
-            # descending
-            while j >= 0 and key_height > heights[j]:
-                heights[j + 1] = heights[j]
-                names[j + 1] = names[j]
-                j -= 1
-            heights[j + 1] = key_height
-            names[j + 1] = key_name
-        return names
+        maxi = max(heights)
+        count_arr = [0] * (maxi + 1)
+        for h in heights:
+            count_arr[h] += 1
+
+        sorted_heights = []
+        for h in range(maxi, -1, -1):
+            while count_arr[h] > 0:
+                sorted_heights.append(h)
+                count_arr[h] -= 1
+        # /after the counting sort map them with height
+        height_name = dict(zip(heights, names)) # height : name
+        # print(height_name) {180: 'Mary', 165: 'John', 170: 'Emma'}
+        return [height_name[height] for height in sorted_heights]
+        
