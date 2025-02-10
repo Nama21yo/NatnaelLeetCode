f8bf91843ab3ef77867a7379f02ee029544f5839
Natnael Yohanes <mataniayohanes@gmail.com>
2025-02-10 15:39:11 +0300

    Time: 297 ms (9.08%), Space: 18.3 MB (41.17%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,15 +1,18 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         n = len(heights)
-        # Bubble Sort
-        for i in range(n):
-            swapped = False
-            for j in range(n - i - 1):
-                if heights[j] < heights[j + 1]:
-                    # swap them
-                    heights[j], heights[j + 1] = heights[j + 1], heights[j]
-                    names[j], names[j + 1] = names[j + 1], names[j]
-                    swapped = True
-            if not swapped:
-                break
+        # Insertion Sort
+        # first part is sorted
+        for i in range(1,n):
+            key_height = heights[i]
+            key_name = names[i]
+
+            j = i - 1
+            # descending
+            while j >= 0 and key_height > heights[j]:
+                heights[j + 1] = heights[j]
+                names[j + 1] = names[j]
+                j -= 1
+            heights[j + 1] = key_height
+            names[j + 1] = key_name
         return names
