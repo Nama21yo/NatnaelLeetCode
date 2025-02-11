6a21488babbe5323f4ea35c43ae04bbea646024a
Natnael Yohanes <mataniayohanes@gmail.com>
2025-02-11 19:15:42 +0300

    Time: 0 ms (100%), Space: 17.6 MB (82.76%) - LeetHub

---
+++ 1009-pancake-sorting/1009-pancake-sorting.py
@@ -0,0 +1,11 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        result = []
+        for i in range(len(arr), 1,-1):
+            idx = arr.index(i) 
+            result.extend([idx + 1, i])
+            arr = arr[:idx:-1] + arr[:idx]
+        return result
+
+
+        
