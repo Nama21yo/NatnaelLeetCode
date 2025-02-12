095b99b6f0eab4960ed5622544cfd63087abd992
Natnael Yohanes <mataniayohanes@gmail.com>
2025-02-12 05:00:38 +0300

    Time: 0 ms (100%), Space: 17.8 MB (41.58%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,33 +1,25 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-         # Calculate the initial gap
-        gap = math.ceil((m + n) / 2)
+        # merge them starting from the back
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
 
-        def swap_elements(left, right):
-            """Helper function to swap elements if needed."""
-            if left < m and right >= m:
-                if nums1[left] > nums2[right - m]:
-                    nums1[left], nums2[right - m] = nums2[right - m], nums1[left]
-            elif left >= m:
-                if nums2[left - m] > nums2[right - m]:
-                    nums2[left - m], nums2[right - m] = nums2[right - m], nums2[left - m]
+        while i >= 0 and j >= 0:
+            # since we are starting from the end
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                if nums1[left] > nums1[right]:
-                    nums1[left], nums1[right] = nums1[right], nums1[left]
-
-        while gap > 0:
-            left = 0
-            right = left + gap
-
-            while right < (m + n):
-                swap_elements(left, right)
-                left += 1
-                right += 1
-
-            # Recalculate the gap for the next iteration
-            if gap == 1:
-                break
-            gap = math.ceil(gap / 2)
-        # Finally, merge nums2 into nums1
-        nums1[m:] = nums2
-        nums1.sort()  # Sort the final nums1 array
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        # for the rest
+        
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+        
+        return nums1
+        
