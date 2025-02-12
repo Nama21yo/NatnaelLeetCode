3e4a66b83c4a487b169b52f78dfd18ffcc5a8269
Natnael Yohanes <mataniayohanes@gmail.com>
2025-02-12 17:03:43 +0300

    Time: 51 ms (58.8%), Space: 18 MB (80.7%) - LeetHub

---
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        set_s = set()
+        min_substring = 0
+        for char in s:
+            if char not in set_s:
+                set_s.add(char)
+            else:
+                min_substring += 1
+                set_s = set(char)
+        return min_substring + 1
+        
+                
