5934ace4d3b66a543526762c5148827157cf278d
Natnael Yohanes <mataniayohanes@gmail.com>
2025-02-18 15:36:51 +0300

    Time: 33 ms (44.44%), Space: 21.5 MB (32.86%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -1,22 +1,16 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        
-        
-        # Dictionary to store the frequency of cumulative sums
-        prefix_sum_count = defaultdict(int)
-        prefix_sum_count[0] = 1  # There's one way to have a sum of 0, by taking no elements
-        
-        current_sum = 0
-        count = 0
+        prefix_map = defaultdict(int)
+        prefix_map[0] = 1
 
+        running_sum = 0
+        count_subarray = 0
         for num in nums:
-            current_sum += num
+            running_sum += num
+            target = running_sum - goal
+            if target in prefix_map:
+                count_subarray += prefix_map[target]
             
-            # Check if (current_sum - goal) exists in the prefix sums
-            if (current_sum - goal) in prefix_sum_count:
-                count += prefix_sum_count[current_sum - goal]
-            
-            # Update the count of current_sum in the dictionary
-            prefix_sum_count[current_sum] += 1
-        
-        return count
+            # if target not in prefix_map:
+            prefix_map[running_sum] += 1
+        return count_subarray
