cfa5aa041a7a88cc431546d65c4068ac50cb4229
Natnael Yohanes <mataniayohanes@gmail.com>
2025-03-04 14:06:27 +0300

    Time: 0 ms (100%), Space: 17.7 MB (92.53%) - LeetHub

---
+++ 1303-minimum-moves-to-reach-target-score/1303-minimum-moves-to-reach-target-score.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        operations = 0
+        while target > 1:
+            if target % 2 == 0 and maxDoubles > 0:
+                target = target // 2
+                maxDoubles -= 1
+                operations += 1
+            else:
+                if maxDoubles == 0:
+                    operations += target - 1
+                    break # or make target = 1
+                else:
+                    target -= 1
+                    operations += 1
+        return operations
