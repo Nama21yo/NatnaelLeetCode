7e0bc67412d8c906982e09e897a387e9a672d788
Natnael Yohanes <mataniayohanes@gmail.com>
2025-03-13 19:46:36 +0300

    Time: 5073 ms (5.02%), Space: 17.8 MB (61.9%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,28 +1,17 @@
-from typing import List
-
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        # Initialize a 2D array to keep track of cached values
-        cache = [[None] * len(nums) for _ in range(len(nums))]
+        n = len(nums)
+        def helper(arr,i,k, p1):
+            if i == k + 1 or k == 0:
+               return 0
+            if p1:  # using flag
+                sub1 = arr[i] + helper(arr,i + 1, k, False) 
+                sub2 =  arr[k] + helper(arr,i, k - 1, False)
+                return max(sub1, sub2)
+            else:
+                sub1 = helper(arr,i + 1, k, True) - arr[i]
+                sub2 = helper(arr,i, k - 1, True) - arr[k]
+                return min(sub1, sub2)
+        val = helper(nums, 0, n - 1, True)
+        return val >= 0
         
-        # Helper function to calculate the score difference
-        def score(left: int, right: int) -> int:
-            # Base case: If the range is invalid, return 0
-            if left > right:
-                return 0
-            
-            # Check if the value is already computed and stored in the cache
-            if cache[left][right] is not None:
-                return cache[left][right]
-            
-            # Calculate the score difference if the player picks the left element
-            score_taking_left = nums[left] - score(left + 1, right)
-            # Calculate the score difference if the player picks the right element
-            score_taking_right = nums[right] - score(left, right - 1)
-            
-            # Store the computed value in the cache
-            cache[left][right] = max(score_taking_left, score_taking_right)
-            return cache[left][right]
-        
-        # Start the recursive function with the entire array range
-        return score(0, len(nums) - 1) >= 0
