55ba51e95b083a52b0a2371cf73f3559e5fa94db
Natnael Yohanes <mataniayohanes@gmail.com>
2025-03-21 18:14:32 +0300

    Time: 0 ms (100%), Space: 18 MB (40.36%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,24 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # Initialize an empty list to store all subsets
-        subs = []
-        # Initialize an empty list to store the current combination
-        combs = []
-
-        # Define the backtracking function
-        def backtrack(i, combs, subs, nums):
-            # If the index reaches the length of nums, add the current combination to subs
+        ans = []
+        def subsequence(nums, i, output):
             if i == len(nums):
-                subs.append(combs.copy())
+                # since it is pass by reference copy it
+                ans.append(output.copy())
                 return
-            # Include the current number in the combination and recurse
-            combs.append(nums[i])
-            backtrack(i + 1, combs, subs, nums)
-            
-            # Exclude the current number from the combination and recurse
-            combs.pop()
-            backtrack(i + 1, combs, subs, nums)
-
-        # Start the backtracking with index 0
-        backtrack(0, combs, subs, nums)
-        return subs
+            # include
+            output.append(nums[i])
+            subsequence(nums, i + 1, output)
+            # exclude while backtracking
+            output.pop()
+            subsequence(nums, i + 1, output)
+        subsequence(nums, 0, [])
+        return ans
