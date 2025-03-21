1d152c62a51d9732f1b5c61f079a040f6979bcbf
Natnael Yohanes <mataniayohanes@gmail.com>
2025-03-21 19:24:25 +0300

    Time: 0 ms (100%), Space: 18 MB (56.7%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,27 +1,15 @@
-from typing import List
-
 class Solution:
-    # Helper function to generate permutations
-    def helper(self, res: List[int], nums: List[int], ans: List[List[int]]):
-    # If the current permutation is complete (same length as nums), add it to the answer list
-        if len(nums) == len(res):
-            ans.append(res.copy())  # Use copy to avoid reference issues
-            return
-        
-        # Iterate through each number in nums
-        for i in range(len(nums)):
-            # Skip if the number is already in the current permutation
-            if nums[i] in res:
-                continue
-            # Include the current number in the permutation
-            res.append(nums[i])
-            # Recur to build the next part of the permutation
-            self.helper(res, nums, ans)
-            # Backtrack by removing the last added number
-            res.pop()
-    
-    # Function to initiate the permutation generation
     def permute(self, nums: List[int]) -> List[List[int]]:
-        ans = []  # List to store all permutations
-        self.helper([], nums, ans)  # Start the helper with an empty current permutation
-        return ans  # Return the list of all permutations
+        ans = []
+        def permutations(path, i):
+            # base case
+            if i == len(path) - 1:
+                ans.append(path[:])
+                return
+            # rec case
+            for j in range(i , len(path)):
+                path[i], path[j] = path[j], path[i]
+                permutations(path, i + 1)
+                path[i], path[j] = path[j], path[i]
+        permutations(nums, 0)
+        return ans
