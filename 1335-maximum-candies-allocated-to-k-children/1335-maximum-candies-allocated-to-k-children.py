794407c3e24efc35d51b813a733bf779cc9afa11
Natnael Yohanes <mataniayohanes@gmail.com>
2025-03-29 16:13:31 +0300

    Time: 345 ms (64.64%), Space: 29.8 MB (47.97%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        l = 0
+        r = max(candies) + 1
+        def feasible(mid):
+            count = 0
+            for candy in candies:
+                count += (candy // mid)
+            return count >= k
+        while r - l > 1:
+            mid = l + (r - l) // 2
+            if feasible(mid):
+                l = mid
+            else:
+                r = mid 
+        return l
