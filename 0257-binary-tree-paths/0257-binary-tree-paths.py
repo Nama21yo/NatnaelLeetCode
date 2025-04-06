213a0f0be19cfddd9f00cbc9235034657f24b991
Natnael Yohanes <mataniayohanes@gmail.com>
2025-04-06 10:36:09 +0300

    Time: 0 ms (100%), Space: 17.9 MB (24.63%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -6,28 +6,16 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        # Initialize an empty list to store the paths
-        arr = []
-        # Initialize an empty list to keep track of the current path
-        s = []
-        
-        def backtrack(root, arr, s):
-            # If the current node is not None
-            if root:
-                # Append the value of the current node to the path
-                s.append(str(root.val))
-                # If it's a leaf node (no left or right children)
-                if not root.right and not root.left:
-                    # Join the current path and add it to the list of paths
-                    arr.append("->".join(s))
-                # Recursively call backtrack on the left child
-                backtrack(root.left, arr, s)
-                # Recursively call backtrack on the right child
-                backtrack(root.right, arr, s)
-                # Remove the current node's value from the path (backtrack)
-                s.pop()
-        
-        # Start the backtracking with the root node
-        backtrack(root, arr, s)
-        # Return the list of paths
-        return arr
+        paths = []
+        def binarypath(root,path):
+            if root is None:
+                return
+            path.append(str(root.val))
+            if root.left is None and root.right is None:
+                paths.append("->".join(path))
+            else:
+                binarypath(root.left, path)
+                binarypath(root.right, path)
+            path.pop()
+        binarypath(root, [])
+        return paths
