8a086daa29a07e4d053b946360f1241276d8551f
Natnael Yohanes <mataniayohanes@gmail.com>
2025-04-28 20:10:51 +0300

    Time: 616 ms (27.44%), Space: 29.1 MB (89.44%) - LeetHub

---
+++ 2094-remove-stones-to-minimize-the-total/2094-remove-stones-to-minimize-the-total.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        total = sum(piles)
+        piles = [-x for x in piles]
+        heapify(piles) # in place
+        for i in range(k):
+            max_ = -1 * heappop(piles)
+            removed = (max_ // 2)
+            total = total - removed
+            heappush(piles, (-1 * (max_ - removed)))
+        return total
