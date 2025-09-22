b4c028f0229328fdfceef66da17450216665db09
Natnael Yohanes <mataniayohanes@gmail.com>
2025-09-22 18:05:51 +0300

    Time: 33 ms (87.1%), Space: 17.7 MB (40.75%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,24 +1,16 @@
 class Solution:
-    def __init__(self):
-        self.fibArray = None  # Declare the memoization array
-
     def fib(self, n: int) -> int:
-        # Initialize the memoization array to store computed Fibonacci values
-        self.fibArray = [-1] * (n + 1)
-        
-        # Call the recursive helper function
-        return self.fibbo(n)
+        memo = {}
 
-    def fibbo(self, n: int) -> int:
-        # Base cases
-        if n <= 1:
-            self.fibArray[n] = n
-            return n
+        def _fib_memo(num):
+            if num in memo:
+                return memo[num]
+            if num <= 1:
+                return num
+            
+            sub_problem = _fib_memo(num - 1) + _fib_memo(num - 2)
+            memo[num] = sub_problem
+
+            return sub_problem
         
-        # Memoization: If already computed, return the stored value
-        if self.fibArray[n] != -1:
-            return self.fibArray[n]
-        
-        # Recursive case with memoization
-        self.fibArray[n] = self.fibbo(n - 1) + self.fibbo(n - 2)
-        return self.fibArray[n]
+        return _fib_memo(n)
